623e69c120acbdea31898d25b145997fd84e597d
tatialveso <tatiialveso@gmail.com>
2021-08-27 11:46:37 -0300

    :recycle: version 2.0 out now

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,19 @@
-document.querySelectorAll("#projectImg").addEventListener("mouseover", mouseOver);
-document.querySelectorAll("#projectImg").addEventListener("mouseout", mouseOut);
+//Get the button:
+mybutton = document.getElementById("btn");
 
-function mouseOver() {
-  document.querySelectorAll("#projectImg").style.objectPosition = "bottom";
-  document.querySelectorAll("#projectImg").style.transition = "3s";
-}
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-function mouseOut() {
-  document.querySelectorAll("#projectImg").style.objectPosition = "top";
-}
-
-$(document).ready(function () {
-  let nav_offset_top = $('.header_area').height() + 50;
-
-  function navbarFixed() {
-    if ($('.header_area').length) {
-      $(window).scroll(function () {
-        let scroll = $(window).scrollTop();
-        if (scroll >= nav_offset_top) {
-          $('.header_area .main-menu').addClass('navbar_fixed');
-        } else {
-          $('.header_area .main-menu').removeClass('navbar_fixed');
-        }
-      })
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+}
 
-  navbarFixed();
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
